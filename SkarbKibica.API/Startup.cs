1b3ca65c6f032f22bbf1d32ccda5618cb186f77c
Kukulim <beckon@interia.pl>
2020-10-20 19:41:09 +0200

    remove hard coded conection string

--- SkarbKibica.API/Startup.cs
+++ SkarbKibica.API/Startup.cs
@@ -51,9 +51,7 @@ namespace SkarbKibica.API
 
             services.AddDbContext<SkarbKibicaDbContext>(options =>
             {
-                options.UseSqlServer(
-                    //@"Server=(localdb)\mssqllocaldb;Database=SkarbKibicaDB;Trusted_Connection=True;");
-                    @"Server = tcp:skarbkibica-apidbserver.database.windows.net, 1433; Initial Catalog = SkarbKibica-api_db; Persist Security Info = False; User ID = beckon; Password = Haslo_666; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30");
+                options.UseSqlServer(Configuration.GetConnectionString("SkarbDatabase"));
             });
         }
 
